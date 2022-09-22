2bb7366605e24729671ff89852843b0ef7af6b8b
Samsideen-Naeem <naeemsamsideen618@gmail.com>
2022-09-22 14:30:52 +0000

    first commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {createContext, useState} from "react"
+
+
+const authContext= createContext();
+AuthProvider= authContext.Provider;
+
+function AuthContextProvider() {
+    const [success, setSuccess]= useState("false");
+    const [error, setError]= useState("null");
+    const [loading, setloading]= useState("false");
+    const [user, setUser]= useState("null");
+
+    // signup function
+    const authRegister=async (username, email, password)=> {await fetch(url, {
+method: "POST"
+    })
+    }
+
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default AuthContextProvider
